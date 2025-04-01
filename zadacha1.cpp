2b0247a08c098f9fc0e221da785dd78de0fe5720
Pavel Nikolaev <altertumlicher@gmail.com>
2025-04-01 13:06:50 +0500

    fix

--- zadacha1.cpp
+++ zadacha1.cpp
@@ -2,7 +2,9 @@
 
 #define MODE 1
 
-int add(int a, int b) { return a + b; }
+#if MODE 1
+int add(int a, int b){ return a + b; }
+#endif
 
 int main()
 {
@@ -19,11 +21,13 @@ int main()
 
 #elif MODE == 1
 	std::cout << "Работаю в боевом режиме" << std::endl;
+
 	std::cout << "Введите число 1: ";
 	std::cin >> a;
 	std::cout << "Введите число 2: ";
 	std::cin >> b;
 	std::cout << "Результат сложения: " << add(a, b) << std::endl;
+
 
 #else
 	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
